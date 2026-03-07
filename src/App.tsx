8898cd0a39da52be88b39f2b289adbe9f86fb408
fvraasveld <109905238+fvraasveld@users.noreply.github.com>
2026-03-07 18:39:50 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToolsProvider } from './context/ToolsContext';
 import { UserProvider } from './context/UserContext';
